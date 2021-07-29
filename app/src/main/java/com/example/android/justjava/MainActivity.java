1350699861cfa3ba4ad3b297f5ea9bafe013ca0e
John Young <ynyjcxyz@gmail.com>
2021-07-29 03:34:17 -0700

    pracrise differetn view mathod

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,14 +12,16 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    //global variables
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,51 +31,73 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called when the order button is clicked.
+     * Make sure submitOrder method is public type
      */
 
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank you!";
-        displayMessage(priceMessage);
+        CheckBox checkBoxCream = (CheckBox) findViewById(R.id.cream_checkbox);
+        boolean hasWhippedCream = checkBoxCream.isChecked();
 
-        //display(quantity);
-        //displayPrice(quantity*5);
+        CheckBox checkBoxChocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = checkBoxChocolate.isChecked();
+
+        TextView nameView = (TextView) findViewById(R.id.text_input_name);
+        String inputName = nameView.getText().toString();
+
+        int price = calculatePrice();
+        String message = createOrderSummary(inputName,price,hasWhippedCream,hasChocolate);
+        displayMessage(message);
     }
 
+    private int calculatePrice() {
+        int price = quantity * 5 ;
+        return price;
+    }
 
-    int quantity = 2; //global variables
+    /**
+     * Create summary of the order.
+     *
+     * @param price           of the order
+     * @param addWhippedCream is whether or not to add whipped cream to the coffee
+     * @param addChocolate    is whether or not to add chocolate to the coffee
+     * @return text summary
+     */
+
+    private String createOrderSummary(String nameText, int price , boolean addWhippedCream , boolean addChocolate){
+        String summaryInfo =
+                "Name: " + nameText +
+                "\nAdd whipped cream?"+ addWhippedCream +
+                "\nAdd chocolate?"+ addChocolate +
+                "\nQuantity: " + quantity +
+                "\nTotal: " + price +
+                "\nThank you!";
+        return summaryInfo;
+    }
+
+    //Make sure increment and decrement are both public method
     public void increment(View view){
-        quantity = quantity * 2;
-        display(quantity);
+        quantity = quantity + 1;
+        displayQuantity(quantity);
     }
 
     public void decrement(View view){
-        quantity = quantity / 2;
-        if(quantity>0) { display(quantity);}
+        quantity = quantity - 1;
+        if(quantity>0) { displayQuantity(quantity);}
         else {quantity = 1;};
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
     //This method displays the given text on the screen
     private void displayMessage(String message){
-        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView)findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 }
